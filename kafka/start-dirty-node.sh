1133add19e79e19622207e9ae0e43b5ee292cdc5
Marcin <dziarmagam@gmail.com>
2018-11-05 17:14:44 +0100

    nohup for kafka run script

--- kafka/start-dirty-node.sh
+++ kafka/start-dirty-node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd kafka/kafka_2.11-2.0.0
+cd ~/kafka/kafka_2.11-2.0.0
 mkdir logs
-bin/zookeeper-server-start.sh config/zookeeper.properties 2> logs/zookeeper.log &
-bin/kafka-server-start.sh config/server.properties 2> logs/kafka.log &
+nohup ./bin/zookeeper-server-start.sh config/zookeeper.properties > logs/zookeeper.log &
+nohup ./bin/kafka-server-start.sh config/server.properties > logs/kafka.log &
