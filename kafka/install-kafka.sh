6c09bd7533d8cb92422dbfc58b7886255ecbe8bf
Marcin <dziarmagam@gmail.com>
2018-11-05 17:00:12 +0100

    fix for curl

--- kafka/install-kafka.sh
+++ kafka/install-kafka.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-adduser --disabled-password --gecos "" kafka
-su kafka
-cd ~/
-curl http://ftp.man.poznan.pl/apache/kafka/2.0.0/kafka_2.11-2.0.0.tgz
+mkdir kafka
+cd kafka
+wget http://ftp.man.poznan.pl/apache/kafka/2.0.0/kafka_2.11-2.0.0.tgz
 tar -xzf kafka_2.11-2.0.0.tgz
 cd kafka_2.11-2.0.0
-exit
