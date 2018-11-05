8ada0777fc1f680560019edc65ec2f27e98ed145
Marcin <dziarmagam@gmail.com>
2018-11-05 17:01:21 +0100

    no kafka user

--- kafka/start-dirty-node.sh
+++ kafka/start-dirty-node.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-su kafka
 cd ~/kafka_2.11-2.0.0
 mkdir logs
 bin/zookeeper-server-start.sh config/zookeeper.properties 2> logs/zookeeper.log &
 bin/kafka-server-start.sh config/server.properties 2> logs/kafka.log &
-exit
